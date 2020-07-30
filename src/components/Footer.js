5e8da68e384d7dc2ca5418b56b6d79036d9199b8
praneershrest <praneer.shrest@gmail.com>
2020-07-30 10:45:23 -0700

    added footer icons with spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,20 @@ import React from "react";
 import styles from "../css/Footer.module.css";
 
 import facebook from "../icons/facebook.svg";
+import instagram from "../icons/instagram.svg";
+import github from "../icons/github.svg";
 
 export default props => {
   return (
     <div id={styles.container}>
       <a href="https://www.facebook.com/sfusurge">
         <img src={facebook} alt="Link to SFU Surge Facebook Account." />
+      </a>
+      <a href="https://www.instagram.com/sfusurge">
+        <img src={instagram} alt="Link to SFU Surge Instagram Account"/>
+      </a>
+      <a href="https://github.com/sfusurge">
+        <img src={github} alt="Link to SFU Surge Github Account" />
       </a>
     </div>
   )
