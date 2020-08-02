8bb19a2480377e6e6d42d9cd9fa0ad455d0f1726
alikhamesy <alikhamesy0@gmail.com>
2020-08-01 17:40:05 -0700

    added react-router and made commented out routes for home, projects, events, about

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-function App() {
+import Footer from './components/Footer'
+
+import './App.css'
+
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* <Header /> */}
+      <Router>
+        <Switch>
+          {/* <Route path="/projects">
+            <Projects />
+          </Route> */}
+          {/* <Route path="/events">
+            <Events />
+          </Route> */}
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+          {/* <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+      </Router>
+      <Footer />
+    </>
+  )
 }
-
-export default App;
