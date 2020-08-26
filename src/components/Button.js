b79faca8f70f4cd297055a44f744287c467bf650
praneershrest <praneer.shrest@gmail.com>
2020-08-26 08:19:01 -0700

    created a general button component and send in Spotify related details through props as instructed by our lord and savior, Khamesy of the Ali

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "../css/Button.module.css";
+
+export default props => {
+  return (
+    <a id={styles.container} href={props.ref}>
+      <img className={styles.element} src={props.image} alt={props.altText} />
+      <p className={styles.element}>{props.text}</p>
+    </a>
+  )
+}
