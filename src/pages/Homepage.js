eb47345caf17e7886494ccc14976b1865f3220e8
alikhamesy <alikhamesy0@gmail.com>
2020-08-26 15:36:31 -0700

    restructure Homepage Page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Activity from "./Activity";
+import Activity from "../components/Activity";
 
 import scooter from "../assets/scooter.svg";
 import robot from "../assets/robot.svg"
