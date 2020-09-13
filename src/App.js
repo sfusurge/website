ac49d5cb27f26447c7daac7c804d0fc3ac0b4a04
praneershrest <51378040+praneershrest@users.noreply.github.com>
2020-09-13 16:23:49 -0700

    Spotify button (#4)
    * Created Spotify button component
    
    * created a general button component and send in Spotify related details through props as instructed by our lord and savior, Khamesy of the Ali
    
    * removed SpotifyButton component and changed font-weight to 600 from 550
    
    * rename dimentions in Button to styles and change id to classname
    
    * remove old file
    
    * found bug in Activity.module.css styling 'img'
    
    Co-authored-by: alikhamesy <alikhamesy0@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-import Homepage from "./components/Homepage";
+import Homepage from "./pages/Homepage"
+import Button from "./components/Button"
 
 import './App.css';
+
+import spotify from "./icons/spotify-icon.svg"
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-    </div >
+      <Button
+        image={spotify}
+        altText="Link to Spotify"
+        text="Listen on Spotify"
+        link="https://open.spotify.com/show/5GAuw7brERfbZSfnKpURvE?si=f1Uz9jp4T0O9yD1MBqD7Eg"
+        style={{ width: "75%", height: "52px" }}
+      />
+    </div>
   );
 }
 
