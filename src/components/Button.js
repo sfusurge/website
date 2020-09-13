4dd2b9094662d45b97c2ed6c9606e693df1a7282
alikhamesy <alikhamesy0@gmail.com>
2020-09-13 15:51:53 -0700

    rename dimentions in Button to styles and change id to classname

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,8 +4,8 @@ import styles from "../css/Button.module.css";
 
 export default props => {
   return (
-    <a id={styles.container} style={props.dimension} href={props.ref}>
-      <img className={styles.element} src={props.image} alt={props.altText} />
+    <a className={styles.container} style={props.style} href={props.ref}>
+      {props.image && <img className={styles.element} src={props.image} alt={props.altText} />}
       <p className={styles.element}>{props.text}</p>
     </a>
   )
