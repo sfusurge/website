7303f3fc24e098ce58e55cfbef4f06f917b47a27
alikhamesy <alikhamesy0@gmail.com>
2020-09-13 16:22:08 -0700

    found bug in Activity.module.css styling 'img'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-import Homepage from "./components/Homepage"
+import Homepage from "./pages/Homepage"
 import Button from "./components/Button"
 
 import './App.css';
 
+import spotify from "./icons/spotify-icon.svg"
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Homepage />
+      <Button
+        image={spotify}
+        altText="Link to Spotify"
+        text="Listen on Spotify"
+        link="https://open.spotify.com/show/5GAuw7brERfbZSfnKpURvE?si=f1Uz9jp4T0O9yD1MBqD7Eg"
+        style={{ width: "75%", height: "52px" }}
+      />
     </div>
   );
 }
