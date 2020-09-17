9a3cba52f3382667757f03a4ad4f9db36d612c45
alikhamesy <alikhamesy0@gmail.com>
2020-09-17 01:22:03 -0700

    updated footer component to accept children for styled footer like seen at the bottom of projects page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,19 @@ import github from "../icons/github.svg"
 
 export default props => {
   return (
-    <div id={styles.footer}>
-      <a href="https://www.facebook.com/sfusurge" className={styles.iconLink}>
-        <img className={styles.icon} src={facebook} alt="Facebook Page" />
-      </a>
-      <a href="https://www.instagram.com/sfusurge/" className={styles.iconLink}>
-        <img className={styles.icon} src={instagram} alt="Instagram Page" />
-      </a>
-      <a href="https://github.com/sfusurge" className={styles.iconLink}>
-        <img className={styles.icon} src={github} alt="Github Page" />
-      </a>
+    <div id={styles.container}>
+      {props.children || <div id={styles.defualt}></div>}
+      <div id={styles.footer}>
+        <a href="https://www.facebook.com/sfusurge" className={styles.iconLink}>
+          <img className={styles.icon} src={facebook} alt="Facebook Page" />
+        </a>
+        <a href="https://www.instagram.com/sfusurge/" className={styles.iconLink}>
+          <img className={styles.icon} src={instagram} alt="Instagram Page" />
+        </a>
+        <a href="https://github.com/sfusurge" className={styles.iconLink}>
+          <img className={styles.icon} src={github} alt="Github Page" />
+        </a>
+      </div>
     </div>
   )
 } 
