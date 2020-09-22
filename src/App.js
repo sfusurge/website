58490bdf943bb4b530330d00566f3f8b8311dae7
alikhamesy <alikhamesy0@gmail.com>
2020-09-21 19:09:57 -0700

    update app.js to include Projects component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
+import Projects from "./pages/Projects"
 import Button from "./components/Button"
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 
 import './App.css';
 
-import Projects from "./pages/Projects"
+import spotify from "./icons/spotify-icon.svg"
 
 function App() {
   const [scroll, scrollLock] = useState(true)
 
   return (
-    <div className="App">
+    <div id="app">
       <Router>
         <Switch>
           <Route path="/projects">
