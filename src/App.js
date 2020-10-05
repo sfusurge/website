3eaf97eea9370b5a303c92785244fd3d0dbcfede
alikhamesy <40162664+alikhamesy@users.noreply.github.com>
2020-10-04 22:44:44 -0700

    Projects page and Project card component (#10)
    * started work on projects
    
    * adding pngs for pacmacro project
    
    * finished the Project component
    
    * added project splash, description and application timer to Projects page. Modified App.css to add background-color to css var
    
    * made the 'apply here' button disapear if application deadline has passed
    
    * forgot to add the application link woops
    
    * adding empty space above prev. projects when applications are closed
    
    * CSS hell, when every project is unique ;(
    
    * update app.js to include Projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
+import Projects from "./pages/Projects"
 import About from './pages/About'
 import Button from "./components/Button"
 import Nav from "./components/Nav"
@@ -18,9 +19,9 @@ function App() {
     <div id="app">
       <Router>
         <Switch>
-          {/* <Route path="/projects">
+          <Route path="/projects">
             <Projects />
-          </Route> */}
+          </Route>
           {/* <Route path="/events">
             <Events />
           </Route> */}
