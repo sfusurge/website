977a7f9e98b321ef42e4dc95657ccda6706675a4
alikhamesy <alikhamesy0@gmail.com>
2020-10-21 02:12:41 -0700

    added working dropdown component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
-import Projects from "./pages/Projects"
 import About from './pages/About'
+// import Events from "./pages/Events"
+
 import Button from "./components/Button"
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
@@ -19,9 +20,9 @@ function App() {
     <div id="app">
       <Router>
         <Switch>
-          <Route path="/projects">
+          {/* <Route path="/projects">
             <Projects />
-          </Route>
+          </Route> */}
           {/* <Route path="/events">
             <Events />
           </Route> */}
