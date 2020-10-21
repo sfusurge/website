a63dbfe9585efd2af06fcf28778c5fdc7148c295
praneershrest <praneer.shrest@gmail.com>
2020-10-20 21:27:05 -0700

    cleaned App.js so merging is easier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import Homepage from "./pages/Homepage"
-import About from './pages/About'
-import Button from "./components/Button"
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-
 import './App.css';
 
 import spotify from "./icons/spotify-icon.svg"
@@ -16,29 +10,6 @@ function App() {
 
   return (
     <div id="app">
-      <Router>
-        <Switch>
-          {/* <Route path="/projects">
-            <Projects />
-          </Route> */}
-          {/* <Route path="/events">
-            <Events />
-          </Route> */}
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
-      </Router>
-      <Button
-        image={spotify}
-        altText="Link to Spotify"
-        text="Listen on Spotify"
-        link="https://open.spotify.com/show/5GAuw7brERfbZSfnKpURvE?si=f1Uz9jp4T0O9yD1MBqD7Eg"
-        style={{ width: "75%", height: "52px" }}
-      />
     </div>
   );
 }
