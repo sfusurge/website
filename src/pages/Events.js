13baa9bd72d35017bb817910e9631a13247f41a0
alikhamesy <alikhamesy0@gmail.com>
2020-10-31 22:05:13 -0700

    Events page done

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -9,6 +9,13 @@ import Dropdown from "../components/Dropdown"
 
 import splash from "../assets/events-splash.svg"
 
+const fills = [
+  { body: "#3D73C7", accent: "#2B5699" },
+  { body: "#6D97D5", accent: "#447CCE" },
+  { body: "#C5D0E2", accent: "#ADBED8" },
+  { body: "#8BA7E6", accent: "#6B8FE0" }
+]
+
 export default props => {
   const [category, setCategory] = useState(EventCategories.all)
 
@@ -22,7 +29,7 @@ export default props => {
       <p id={styles.text}>Events are a core component of our semesterly activities.  On a regular basis, we host talks, workshops, socials, and more. Together, we also tend to do a lot of Leetcode problems. Scroll down below to see what events we have coming up. We hope to see you there!</p>
       <h1 id={styles.header}>Upcoming Events</h1>
 
-      <Dropdown options={Object.values(EventCategories)} onClick={onFilter} classes={{dropdownStyles: styles.dropdown}} />
+      <Dropdown options={Object.values(EventCategories)} onClick={onFilter} classes={{ dropdownStyles: styles.dropdown }} />
 
       {events.map((event, index) => {
         return (category === EventCategories.all || category === event.category) &&
@@ -36,6 +43,7 @@ export default props => {
             }}
             time={event.time}
             link={event.link}
+            fill={fills[index % 4]}
             key={event.title + index}
           />
       })}
